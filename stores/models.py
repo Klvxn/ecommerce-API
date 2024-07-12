9d57b626b128cab8df907ca1cc9431f61ac84a3f
Klvn <akpulukelvin@gmail.com>
2024-07-12 14:24:55 +0100

    followers count

--- stores/models.py
+++ stores/models.py
@@ -1,7 +1,7 @@
 from autoslug import AutoSlugField
 from django.db import models
 
-from customers.models import Customer
+from customers.models import Customer, Address
 
 
 # Create your models here.
@@ -19,10 +19,16 @@ class Store(models.Model):
     followers = models.ManyToManyField(Customer, related_name="following")
     slug = AutoSlugField(populate_from="brand_name", unique=True, always_update=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+    address = models.OneToOneField(Address, on_delete=models.RESTRICT, null=True)
 
     def __str__(self):
         return self.brand_name
 
+    @property
+    def followers_count(self):
+        return self.followers.count()
+
+    @property
     def products_count(self):
         return self.product_set.all().count()
 
