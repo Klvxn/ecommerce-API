ac4f28a41b0554f61f9998a763ad12ce76e75b3e
klvn <akpulukelvin@gmail.com>
2025-01-04 23:21:21 +0100

    minor changes & optimizations 2

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -3,8 +3,15 @@ from django.contrib.admin.options import BaseModelAdmin
 from django.contrib.auth.models import AnonymousUser
 from django.utils.translation import ngettext
 
-from .models import (Category, Product, ProductAttribute, ProductAttributeValue,
-                     ProductImage, Review, ReviewImage)
+from .models import (
+    Category,
+    Product,
+    ProductAttribute,
+    ProductAttributeValue,
+    ProductMedia,
+    Review,
+    ReviewImage,
+)
 from .forms import AttributeModelForm, OfferModelForm, VoucherModelForm
 from .vouchers.models import Offer, Voucher, RedeemedVoucher
 
@@ -12,13 +19,11 @@ from .vouchers.models import Offer, Voucher, RedeemedVoucher
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-
     list_display = ["name", "slug"]
     prepopulated_fields = {"slug": ["name"]}
 
 
 class SharedPermMixin(BaseModelAdmin):
-
     def has_view_permission(self, request, obj=None):
         return self.has_module_permission(request)
 
@@ -26,9 +31,7 @@ class SharedPermMixin(BaseModelAdmin):
         return self.has_module_permission(request)
 
     def has_change_permission(self, request, obj=None):
-        return (False
-                if not obj
-                else obj.product.store.owner == request.user or request.user.is_superuser)
+        return False if not obj else obj.product.store.owner == request.user or request.user.is_superuser
 
     def has_delete_permission(self, request, obj=None):
         return self.has_change_permission(request, obj)
@@ -40,7 +43,6 @@ class SharedPermMixin(BaseModelAdmin):
 
 
 class ProductAttributeValueInline(admin.TabularInline, SharedPermMixin):
-
     model = ProductAttributeValue
     min_num = 1
     extra = 2
@@ -48,24 +50,21 @@ class ProductAttributeValueInline(admin.TabularInline, SharedPermMixin):
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        return qs if request.user.is_superuser else qs.filter(
-            attribute__product__store__owner=request.user
+        return (
+            qs if request.user.is_superuser else qs.filter(attribute__product__store__owner=request.user)
         )
 
 
 @admin.register(ProductAttribute)
 class ProductAttributeAdmin(admin.ModelAdmin, SharedPermMixin):
-
     list_display = ["name", "product", "attribute_values"]
     inlines = [ProductAttributeValueInline]
     form = AttributeModelForm
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        return qs if request.user.is_superuser else qs.filter(
-            product__store__owner=request.user
-        )
-    
+        return qs if request.user.is_superuser else qs.filter(product__store__owner=request.user)
+
     def get_form(self, request, obj=None, change=False, **kwargs):
         form = super().get_form(request, obj, change, **kwargs)
         form.set_current_user(request.user)
@@ -73,16 +72,14 @@ class ProductAttributeAdmin(admin.ModelAdmin, SharedPermMixin):
 
 
 class ProductAttributeInline(admin.TabularInline):
-
     model = ProductAttribute
     min_num = 1
     extra = 2
     fk_name = "product"
 
 
-class ProductImageInline(admin.StackedInline):
-
-    model = ProductImage
+class ProductMediaInline(admin.StackedInline):
+    model = ProductMedia
     fk_name = "product"
     exclude = ["id"]
     readonly_fields = ["image_url"]
@@ -91,7 +88,6 @@ class ProductImageInline(admin.StackedInline):
 
 
 class ReviewImageInline(admin.StackedInline):
-
     model = ReviewImage
     fk_name = "review"
     exclude = ["id"]
@@ -102,7 +98,6 @@ class ReviewImageInline(admin.StackedInline):
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-
     model = Review
     exclude = ["id"]
     list_display = ["__str__", "product", "created", "rating"]
@@ -112,16 +107,13 @@ class ReviewAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        return qs if request.user.is_superuser else qs.filter(
-            product__store__owner=request.user
-        )
+        return qs if request.user.is_superuser else qs.filter(product__store__owner=request.user)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-
     actions = ["make_unavailable"]
-    inlines = [ProductImageInline, ProductAttributeInline]
+    inlines = [ProductMediaInline, ProductAttributeInline]
     list_display = ["name", "category", "in_stock", "store", "available"]
     readonly_fields = ["rating", "quantity_sold"]
     list_editable = ["category", "available", "in_stock"]
@@ -135,7 +127,7 @@ class ProductAdmin(admin.ModelAdmin):
             return qs
         return qs.filter(store__owner=request.user)
 
-    @admin.action(description='Mark selected products as unavailable')
+    @admin.action(description="Mark selected products as unavailable")
     def make_unavailable(self, request, queryset):
         updated = queryset.update(available=False)
         self.message_user(
@@ -143,15 +135,14 @@ class ProductAdmin(admin.ModelAdmin):
             ngettext(
                 f"{updated} product has been marked as unavailable",
                 f"{updated} products have been marked as unavailable",
-                updated
+                updated,
             ),
-            messages.SUCCESS
+            messages.SUCCESS,
         )
 
 
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
-
     list_display = ["__str__", "store", "available_to", "discount_type", "has_expired"]
     readonly_fields = ["store"]
     form = OfferModelForm
@@ -168,16 +159,13 @@ class OfferAdmin(admin.ModelAdmin):
 
 @admin.register(Voucher)
 class VoucherAdmin(admin.ModelAdmin):
-
     form = VoucherModelForm
     list_display = ["__str__", "code", "offer", "usage_type", "num_of_usage"]
     readonly_fields = ["num_of_usage"]
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        return qs if request.user.is_superuser else qs.filter(
-            offer__store__owner=request.user
-        )
+        return qs if request.user.is_superuser else qs.filter(offer__store__owner=request.user)
 
     def get_form(self, request, obj=None, change=False, **kwargs):
         form = super().get_form(request, obj, change, **kwargs)
@@ -187,12 +175,9 @@ class VoucherAdmin(admin.ModelAdmin):
 
 @admin.register(RedeemedVoucher)
 class RedeemedVoucherAdmin(admin.ModelAdmin):
-
     list_display = ["voucher", "customer", "date_redeemed"]
     readonly_fields = list_display
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        return qs if request.user.is_superuser else qs.filter(
-            voucher__offer__store__owner=request.user
-        )
+        return qs if request.user.is_superuser else qs.filter(voucher__offer__store__owner=request.user)
--- catalogue/vouchers/models.py
+++ catalogue/vouchers/models.py
@@ -5,16 +5,19 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AnonymousUser
 from django.db import models
 
-from ..models import BaseModel
+from ..models import Timestamp
 
 
-class Offer(BaseModel):
+class Offer(Timestamp):
     """
     Represents a discount offer model. Offers can be applied to products or shipping fees,
     and can be available to all users, first time buyers or those with voucher code.
     """
+
     FREE_SHIPPING, PERCENTAGE_DISCOUNT, FIXED_DISCOUNT = (
-        "Free shipping", "Percentage discount", "Fixed discount"
+        "Free shipping",
+        "Percentage discount",
+        "Fixed discount",
     )
 
     DISCOUNT_TYPE_CHOICES = (
@@ -34,7 +37,7 @@ class Offer(BaseModel):
     TARGET = (
         # If the offer is being applied to a product's price or the shipping fee
         ("Product", "product"),
-        ("Shipping", "shipping")
+        ("Shipping", "shipping"),
     )
 
     title = models.CharField(max_length=50)
@@ -48,9 +51,7 @@ class Offer(BaseModel):
     total_discount_offered = models.DecimalField(
         max_digits=10, decimal_places=2, default=D(0.0), blank=True
     )
-    min_order_value = models.DecimalField(
-        max_digits=10, decimal_places=2, null=True, blank=True
-    )
+    min_order_value = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     eligible_products = models.ManyToManyField("catalogue.Product", blank=True)
     claimed = models.ManyToManyField("customers.Customer", blank=True)
@@ -63,9 +64,7 @@ class Offer(BaseModel):
 
     def clean(self):
         if self.for_product and self.is_free_shipping:
-            raise ValidationError({
-                "target": "Free shipping can only be applied as a shipping discount"
-            })
+            raise ValidationError({"target": "Free shipping can only be applied as a shipping discount"})
         super().clean()
 
     @property
@@ -132,11 +131,12 @@ class Offer(BaseModel):
         return self.available_to == self.VOUCHERS
 
 
-class Voucher(BaseModel):
+class Voucher(Timestamp):
     """
     Represents a voucher in the system. Vouchers are tied to offers and can have different
     usage types
     """
+
     SINGLE, MULTIPLE, ONCE_PER_CUSTOMER = "single", "multiple", "once per customer"
 
     VOUCHER_USAGE = (
@@ -185,8 +185,7 @@ class Voucher(BaseModel):
 
     def within_validity_period(self):
         return (
-            not self.offer.has_expired and
-            self.valid_from < datetime.now(timezone.utc) <= self.valid_to
+            not self.offer.has_expired and self.valid_from < datetime.now(timezone.utc) <= self.valid_to
         )
 
     def is_valid(self, customer=None, order_value=None):
@@ -197,9 +196,9 @@ class Voucher(BaseModel):
         if not self.offer.above_min_purchase(order_value):
             return False, "Your order is below the required minimum purchase"
         return (
-                self.within_usage_limits(customer) and
-                self.offer.above_min_purchase(order_value) and
-                self.within_validity_period()
+            self.within_usage_limits(customer)
+            and self.offer.above_min_purchase(order_value)
+            and self.within_validity_period()
         ), "Voucher is valid"
 
     def redeem(self, customer, order_value):
@@ -212,8 +211,8 @@ class Voucher(BaseModel):
         )
         self.save()
 
-class RedeemedVoucher(models.Model):
 
+class RedeemedVoucher(models.Model):
     voucher = models.ForeignKey(Voucher, on_delete=models.CASCADE)
     customer = models.ForeignKey("customers.Customer", on_delete=models.CASCADE)
     date_redeemed = models.DateTimeField(auto_now_add=True)
@@ -221,5 +220,3 @@ class RedeemedVoucher(models.Model):
 
     def __str__(self):
         return f"{self.voucher} redeemed by {self.customer}"
-
-
